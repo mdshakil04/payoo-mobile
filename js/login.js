b1d44db3e3ecbf93ff129536edb0953dd5de8abb
mdshakil04 <m.shakil04@yahoo.com>
2025-05-10 23:30:17 +0600

    JS file added

---
+++ js/login.js
@@ -0,0 +1,5 @@
+// console.log('From Login.js file')
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();// রিলোড জেনো না হয়, সেই জন্য এটা দেওয়া হয়
+    console.log('login-btn clicked')
+})
