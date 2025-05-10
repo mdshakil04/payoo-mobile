3709d6721d1a3fc6cc65c83de98c55d8f7468f4c
mdshakil04 <m.shakil04@yahoo.com>
2025-05-11 00:08:29 +0600

    All for today

--- js/login.js
+++ js/login.js
@@ -1,5 +1,7 @@
 // console.log('From Login.js file')
 document.getElementById('login-btn').addEventListener('click', function(event){
     event.preventDefault();// রিলোড জেনো না হয়, সেই জন্য এটা দেওয়া হয়
-    console.log('login-btn clicked')
+    // console.log('login-btn clicked')
+    const phoneNumber = document.getElementById('phone-number');
+    console.log(phoneNumber.value)
 })
