7f0532f5707854dddab2e6c03661c82736f72ed9
mdshakil04 <m.shakil04@yahoo.com>
2025-05-12 21:31:03 +0600

    Login page done

--- js/login.js
+++ js/login.js
@@ -1,7 +1,14 @@
 // console.log('From Login.js file')
-document.getElementById('login-btn').addEventListener('click', function(event){
-    event.preventDefault();// রিলোড জেনো না হয়, সেই জন্য এটা দেওয়া হয়
-    // console.log('login-btn clicked')
-    const phoneNumber = document.getElementById('phone-number');
-    console.log(phoneNumber.value)
-})
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // রিলোড জেনো না হয়, সেই জন্য এটা দেওয়া হয়
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById('pin-number').value;
+    // This is temporary method, you should not do this
+    if(phoneNumber === '01714769575' && pinNumber === '1234'){
+       window.location.href = '/home.html'
+    }else{
+        alert('Your phone or password is incorrect')
+    }
+  });
