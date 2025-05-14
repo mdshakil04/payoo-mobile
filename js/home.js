08e2c283bea814d45a2ea9eda81aa66dcc4a034b
mdshakil04 <m.shakil04@yahoo.com>
2025-05-14 22:30:32 +0600

    Add money functionality done

--- js/home.js
+++ js/home.js
@@ -7,5 +7,18 @@ document
     const inputAddMoney = document.getElementById("input-add-money").value;
     const pinNumber = document.getElementById('pin-number').value;
     // console.log(inputAddMoney, pinNumber)
-    
+    if(pinNumber === "1234"){
+        // console.log('You will add money')
+        const currentBalance = document.getElementById('account-valance').innerText;
+        // console.log(currentBalance)
+        const addMoneyNumber = parseFloat(inputAddMoney);
+        const currentMoneyNumber = parseFloat(currentBalance);
+        const updatedBalance = currentMoneyNumber + addMoneyNumber;
+        // console.log(updatedBalance)
+        // currentBalance.innerText = updatedBalance
+        document.getElementById('account-valance').innerText = updatedBalance;
+
+    }else{
+        alert('Pin is incorrect')
+    }
   });
