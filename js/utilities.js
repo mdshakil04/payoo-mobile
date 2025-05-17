61b03de5fd0a1bb55a1ff6e794b3e18af8ac68a2
mdshakil04 <m.shakil04@yahoo.com>
2025-05-17 16:30:05 +0600

    Cash Out Functionality implemented

--- js/utilities.js
+++ js/utilities.js
@@ -3,7 +3,7 @@
 //     e.preventDefault();
 //     const {money, pin} = getInputData('input-add-money', 'pin-number');
 //     console.log(money, pin);
-    
+
 // })
 // // Reusable function for getting input field data
 // function getInputData(moneyID,pinID){
@@ -12,17 +12,41 @@
 //     const pin = document.getElementById(pinID).value;
 //     return {money, pin};
 // }
-// ---------------------------------------
-document.getElementById('add-money-btn').addEventListener('click', function(event){
+// ------------------Add Money---------------------
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    // console.log('btn clicked')
-    const addMoney = getInputData('input-add-money');
-    const pinNumber = getInputData('pin-number')
-    console.log(typeof(addMoney), typeof(pinNumber))
-})
+    const addMoney = getInputData("money");
+    const pinNumber = getInputData("pin-number");
+    const accountBalance = document.getElementById("account-valance").innerText;
+    const balanceNumber = parseFloat(accountBalance);
+    if (pinNumber === 1234) {
+      const totalBalance = balanceNumber + addMoney;
+      document.getElementById("account-valance").innerText = totalBalance;
+    } else {
+      alert("pin is incorrect!");
+    }
+  });
+// -----------------Cash Out------------------------------
+document
+  .getElementById("cash-out-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const cashOut = getInputData("money");
+    const pinNumber = getInputData("pin-number");
+    const accountBalance = document.getElementById("account-valance").innerText;
+    const balanceNumber = parseFloat(accountBalance);
+    if (pinNumber === 1234 && cashOut < balanceNumber) {
+        const balanceAfterCashOut = balanceNumber - cashOut;
+        document.getElementById('account-valance').innerText = balanceAfterCashOut;
+    }else{
+        alert('pin is incorrect or amount is bigger than total Balance')
+    }
+  });
 // Reusable function
-function getInputData(id){
-    const inputValue = document.getElementById(id).value;
-    const inputValueNumber = parseFloat(inputValue);
-    return inputValueNumber;
+function getInputData(id) {
+  const inputValue = document.getElementById(id).value;
+  const inputValueNumber = parseFloat(inputValue);
+  return inputValueNumber;
 }
