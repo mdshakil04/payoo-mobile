1b51ca59c7bf013d7adb75eb8d271c9d7aeef613
mdshakil04 <m.shakil04@yahoo.com>
2025-05-17 08:32:15 +0600

    Reusable function solved

--- js/utilities.js
+++ js/utilities.js
@@ -1,26 +1,28 @@
 // console.log('Utilities.js')
-document.getElementById('add-money-btn').addEventListener('click', function(e){
-    e.preventDefault()
-    const {money, pin} = getInputData('input-add-money', 'pin-number');
-    console.log(money, pin)
-    
-})
-// Reusable function for getting input field data
-function getInputData(moneyID,pinID){
-    const inputFieldValue = document.getElementById(moneyID).value;
-    const money = parseFloat(inputFieldValue);
-    const pin = document.getElementById(pinID).value;
-    return {money, pin};
-}
-// ---------------------------------------
-// document.getElementById('add-money-btn').addEventListener('click', function(e) {
+// document.getElementById('add-money-btn').addEventListener('click', function(e){
 //     e.preventDefault();
-//     const { money, pin } = getFormData('input-add-money', 'pin-number');
-
-// });
-
-// function getFormData(moneyId, pinId) {
-//     const money = parseFloat(document.getElementById(moneyId).value);
-//     const pin = document.getElementById(pinId).value;
-//     return { money, pin };
+//     const {money, pin} = getInputData('input-add-money', 'pin-number');
+//     console.log(money, pin);
+    
+// })
+// // Reusable function for getting input field data
+// function getInputData(moneyID,pinID){
+//     const inputFieldValue = document.getElementById(moneyID).value;
+//     const money = parseFloat(inputFieldValue);
+//     const pin = document.getElementById(pinID).value;
+//     return {money, pin};
 // }
+// ---------------------------------------
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    // console.log('btn clicked')
+    const addMoney = getInputData('input-add-money');
+    const pinNumber = getInputData('pin-number')
+    console.log(typeof(addMoney), typeof(pinNumber))
+})
+// Reusable function
+function getInputData(id){
+    const inputValue = document.getElementById(id).value;
+    const inputValueNumber = parseFloat(inputValue);
+    return inputValueNumber;
+}
